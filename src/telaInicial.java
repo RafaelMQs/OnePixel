140c1fec8fa326adb9f1df49bb0d04c04eb9122b
RafaelMQs <rafaeelmq08@gmail.com>
2021-09-03 21:26:23 -0300

    Construindo a tela inicial

--- src/telaInicial.java
+++ src/telaInicial.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class telaInicial extends JFrame implements MouseListener, KeyListener {
 	private JLabel fundoLogo;
-	private JButton iniciarJogo, fecharJogo;
+	private JButton iniciarNoSelected, fecharNoSelected, iniciarSelected, fecharSelected;
 
 	public telaInicial() {
 		Componentes();
@@ -16,45 +16,74 @@ public class telaInicial extends JFrame implements MouseListener, KeyListener {
 
 	public void Tela() {
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setSize(600, 250); // Tamanho do JFrame
+		setSize(600, 310); // Tamanho do JFrame
 		setUndecorated(true); // Desativa a Decoração
 		setLocationRelativeTo(null); // Tela Centralizada
 		setVisible(false);
-		getContentPane().setBackground(Color.BLACK);
-		// getContentPane().add(fundoLogo);
+		getContentPane().add(fundoLogo);
 	}
 
 	public void Componentes() {
 		setLayout(null);
 		addKeyListener(this);
+		
 		// INICIO - Definir Fundo
-		// fundoLogo = new JLabel(new ImageIcon("res//logoONEpixel.png"));
-		// fundoLogo.setBounds(0, 0, 600, 250);
+		fundoLogo = new JLabel(new ImageIcon("TelaInicial//BkTelaPrinc.gif"));
+		fundoLogo.setBounds(0, 0, 600, 310);
 		// FIM - Definir Fundo
 
 		// INICIO - Definir Botões
-		iniciarJogo = new JButton("INICIAR JOGO");
-		iniciarJogo.setBounds(200, 100, 200, 30);
-		iniciarJogo.addMouseListener(this); // Adicionando um evento de mouse ( para algum hover talvez )
-		iniciarJogo.setFocusable(false);
-		add(iniciarJogo);
+		
+		// BOTÃO INICIAR NÃO SELECIONADO
+		iniciarNoSelected = new JButton(new ImageIcon("TelaInicial//iniciar01-noSelect.png"));
+		iniciarNoSelected.setBounds(200, 180, 200, 30);
+		iniciarNoSelected.addMouseListener(this); // Adicionando um evento de mouse ( para algum hover talvez )
+		iniciarNoSelected.setFocusable(false);
+		iniciarNoSelected.setContentAreaFilled(false);
+		iniciarNoSelected.setBorderPainted(false);
+		add(iniciarNoSelected);
 
-		fecharJogo = new JButton("FECHAR JOGO");
-		fecharJogo.setBounds(200, 150, 200, 30);
-		fecharJogo.addMouseListener(this); // Adicionando um evento de mouse ( para algum hover talvez )
-		fecharJogo.setFocusable(false);
-		add(fecharJogo);
+		// BOTÃO INICIAR SELECIONADO
+		iniciarSelected = new JButton(new ImageIcon("TelaInicial//iniciar01-Select.png"));
+		iniciarSelected.setBounds(200, 180, 200, 30);
+		iniciarSelected.addMouseListener(this); // Adicionando um evento de mouse ( para algum hover talvez )
+		iniciarSelected.setFocusable(false);
+		iniciarSelected.setContentAreaFilled(false);
+		iniciarSelected.setBorderPainted(false);
+		iniciarSelected.setVisible(false);
+		add(iniciarSelected);
+		
+		// BOTÃO FECHAR NÃO SELECIONADO
+		fecharNoSelected = new JButton(new ImageIcon("TelaInicial//fechar01-noSelect.png"));
+		fecharNoSelected.setBounds(200, 230, 200, 30);
+		fecharNoSelected.addMouseListener(this); // Adicionando um evento de mouse ( para algum hover talvez )
+		fecharNoSelected.setFocusable(false);
+		fecharNoSelected.setContentAreaFilled(false);
+		fecharNoSelected.setBorderPainted(false);
+		add(fecharNoSelected);
+		
+		// BOTÃO FECHAR SELECIONADO
+		fecharSelected = new JButton(new ImageIcon("TelaInicial//fechar01-Select.png"));
+		fecharSelected.setBounds(200, 230, 200, 30);
+		fecharSelected.addMouseListener(this); // Adicionando um evento de mouse ( para algum hover talvez )
+		fecharSelected.setFocusable(false);
+		fecharSelected.setContentAreaFilled(false);
+		fecharSelected.setBorderPainted(false);
+		fecharSelected.setVisible(false);
+		add(fecharSelected);
 		// FIM - Definir Botões
 
 	}
 
 	public void Eventos() {
 		// Botão de Fechar o Jogo
-		fecharJogo.addActionListener(new ActionListener() {
+		fecharSelected.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// Objeto para trocaro Yes e o No do JOptionPane
+				Object[] options = {"Sim", "Não"};
 				// Pergunta se ele tem certeza de que deseja fechar o jogo
-				int resposta = JOptionPane.showConfirmDialog(null, "Tem Certeza ?", "Deseja Sair?",
-						JOptionPane.YES_NO_OPTION);
+				int resposta = JOptionPane.showOptionDialog(null, "Tem Certeza ?", "Deseja Sair?",
+						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,null, options, options[0]);
 
 				if (resposta == JOptionPane.YES_OPTION) {
 					System.exit(0);
@@ -92,20 +121,24 @@ public class telaInicial extends JFrame implements MouseListener, KeyListener {
 	// Aumenta os botões quando o mouse estiver em cima
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		if (e.getSource() == iniciarJogo) {
-			iniciarJogo.setBounds(iniciarJogo.getX() - 6, iniciarJogo.getY() - 6, iniciarJogo.getWidth() + 12, iniciarJogo.getHeight() + 12);
-		} else if (e.getSource() == fecharJogo) {
-			fecharJogo.setBounds(fecharJogo.getX() - 6, fecharJogo.getY() - 6, fecharJogo.getWidth() + 12, fecharJogo.getHeight() + 12);
+		if (e.getSource() == iniciarNoSelected) {
+			iniciarNoSelected.setVisible(false);
+			iniciarSelected.setVisible(true);
+		} else if (e.getSource() == fecharNoSelected) {
+			fecharNoSelected.setVisible(false);
+			fecharSelected.setVisible(true);
 		}
 	}
 
 	// Diminui os botões quando o mouse sair
 	@Override
 	public void mouseExited(MouseEvent e) {
-		if (e.getSource() == iniciarJogo) {
-			iniciarJogo.setBounds(iniciarJogo.getX() + 6, iniciarJogo.getY() + 6, iniciarJogo.getWidth() - 12, iniciarJogo.getHeight() - 12);
-		} else if (e.getSource() == fecharJogo) {
-			fecharJogo.setBounds(fecharJogo.getX() + 6, fecharJogo.getY() + 6, fecharJogo.getWidth() - 12, fecharJogo.getHeight() - 12);
+		if (e.getSource() == iniciarSelected) {
+			iniciarNoSelected.setVisible(true);
+			iniciarSelected.setVisible(false);
+		} else if (e.getSource() == fecharSelected) {
+			fecharNoSelected.setVisible(true);
+			fecharSelected.setVisible(false);
 		}
 	}
 
